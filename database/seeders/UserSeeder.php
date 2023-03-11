0b7f648f900652cd0a7ba2801ea5bde8cef7e81c
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-03-11 23:46:20 +0700

    setup seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Ferdy Hahan Pradana',
+            'email' => 'ferdyhahan5@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $faker = Faker::create('id_ID');
+        for ($i = 1; $i <= 5; $i++) {
+
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('password'),
+            ]);
+        }
+    }
+}
